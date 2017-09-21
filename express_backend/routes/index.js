5868f545a1cbd328f0da45d7f54dbef70ee8fe13
Juan Camilo Bages <jcbages@outlook.com>
2017-09-21 09:17:03 -0500

    added .gitignore & new deps

--- express_backend/routes/index.js
+++ express_backend/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var github = new require('github')({});
+
 /* GET home page. */
-router.get('/followers', function(req, res, next) {
-  res.json({message: 'hola hola'});
+router.get('/followers/:user', function(req, res, next) {
+	github.users.getFollowingForUser({
+    username: req.params.user
+	}, (err, data) => {
+  	res.json(data);
+  });
 });
 
 module.exports = router;
